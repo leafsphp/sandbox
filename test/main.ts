b0b6cb3a70070e2f816d981aefbe7daac1f87b80
Michael Darko <mickdd22@gmail.com>
2022-07-09 10:12:39 +0000

    feat: add working code example

--- test/main.ts
+++ test/main.ts
@@ -1,9 +1,11 @@
-import { createApp, h, watchEffect } from 'vue'
+import { createApp, h, watchEffect, ref } from 'vue'
 import { Repl, ReplStore } from '../src'
+import axios from 'axios'
 ;(window as any).process = { env: {} }
 
 const App = {
   setup() {
+    const output = ref('')
     const query = new URLSearchParams(location.search)
     const store = new ReplStore({
       serializedState: location.hash.slice(1),
@@ -18,6 +20,44 @@ const App = {
     })
 
     watchEffect(() => history.replaceState({}, '', store.serialize()))
+
+    const run = async (files: Record<string, any>) => {
+      const form = new FormData();
+      const rawFiles: any = {};
+
+      Object.keys(files).forEach((filename) => {
+        form.set(filename, files[filename].code);
+        rawFiles[filename] = files[filename].code;
+      });
+
+      let { data: folder } = await axios.post('http://localhost:3600/compile', form);
+
+      if (!folder) {
+        return store.state.errors.push('Internal system error, please try again');
+      } else {
+        store.state.errors = [];
+      }
+  
+      try {
+        let { data } = await axios.get(
+          `http://localhost:3600/${folder.folder}`
+        );
+
+        console.log(rawFiles, data, 'files');
+
+        if (typeof data !== 'string') {
+          data = JSON.stringify(data);
+        }
+
+        output.value = data;
+      } catch (error: any) {
+        if (error.response.data) {
+          store.state.errors.push(error.response.data);
+        } else {
+          store.state.errors.push(error);
+        }
+      }
+    };
 
     // setTimeout(() => {
     // store.setFiles(
@@ -38,6 +78,8 @@ const App = {
       h(Repl, {
         store,
         // layout: 'vertical',
+        output: output.value,
+        run,
         ssr: true,
         sfcOptions: {
           script: {
